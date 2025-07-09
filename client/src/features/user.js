5b50aaf4dc151a78e40d82648a88ea913bb3a243
Colo01 <cvlytoxd@gmail.com>
2025-07-09 13:14:05 -0300

    Fix: URLs y creacion de .env

--- client/src/features/user.js
+++ client/src/features/user.js
@@ -2,14 +2,11 @@
 
 import { createAsyncThunk, createReducer } from "@reduxjs/toolkit";
 import axios from "axios";
-
+const API_URL = process.env.REACT_APP_API_URL;
 export const userRegister = createAsyncThunk("USER_REGISTER", async (data) => {
   console.log("Intentando enviar datos al backend:", data); // Log datos enviados
   try {
-    const response = await axios.post(
-      `${process.env.REACT_APP_API_URL}/api/user/register`,
-      data
-    );
+    const response = await axios.post(`${API_URL}/api/user/register`, data);
     console.log("Respuesta del servidor:", response.data); // Log respuesta exitosa
     return response.data;
   } catch (error) {
@@ -23,21 +20,17 @@ export const userRegister = createAsyncThunk("USER_REGISTER", async (data) => {
 
 export const userLogin = createAsyncThunk("USER_LOGIN", (data) => {
   console.log(data);
-  return axios
-    .post(`${process.env.REACT_APP_API_URL}/api/user/login`, data)
-    .then((user) => {
-      localStorage.setItem("user", JSON.stringify(user.data));
-      return user.data;
-    });
+  return axios.post(`${API_URL}/api/user/login`, data).then((user) => {
+    localStorage.setItem("user", JSON.stringify(user.data));
+    return user.data;
+  });
 });
 
 export const userLogout = createAsyncThunk("USER_LOGOUT", () => {
-  return axios
-    .post(`${process.env.REACT_APP_API_URL}/api/user/logout`)
-    .then(() => {
-      localStorage.removeItem("user");
-      return {};
-    });
+  return axios.post(`${API_URL}/api/user/logout`).then(() => {
+    localStorage.removeItem("user");
+    return {};
+  });
 });
 
 const userReducer = createReducer(
